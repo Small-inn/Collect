e20b8929cf258318b171923024a0b5bdba3ec237
Small_inn <small_inn@163.com>
2017-10-25 17:54:51 +0800

    1025

--- Typescript/04-接口/index.ts
+++ Typescript/04-接口/index.ts
@@ -1,4 +1,6 @@
-
+// 接口定义：
+// TS的核心原则之一是对值所具有的结构进行类型检查。称作：“鸭式辨型法”或者“结构性子类型化”
+// 在TS中接口的作用就是为了这些类型命名和为你的代码或者第三方代码契约。
 // 1.0 接口初探
 function printLabel(labelledObj: { label: string }) {
     console.log(labelledObj.label);
@@ -44,18 +46,108 @@ let p1: Point = { x: 10, y: 20 };
 p1.x = 5;//error
 // 4.0 函数类型
 interface SearchFun {
-    (source:string,subString:string):boolean;
+    (source: string, subString: string): boolean;
 }
-let mySearch:SearchFun;
-mySearch = function(source:string,subString:string){
+let mySearch: SearchFun;
+mySearch = function (source: string, subString: string) {
     let result = source.search(subString);
     return result > -1;
 }
 // 5.0 可索引的类型
 interface stringArray {
-    [index:number]:string;
+    [index: number]: string;
 }
 let myArray: stringArray;
-myArray = ['Bob','Fred'];
-let myStr:string = myArray[0];
+myArray = ['Bob', 'Fred'];
+let myStr: string = myArray[0];
+//  支持两种索引名：字符串和数字。
+// 6.0 类类型
+//  实现接口 TS可以用接口明确的强制一个类去符合某种契约。
+interface ClockInterface {
+    currentTime: Date;
+}
+class Clock implements ClockInterface {
+    currentTime: Date;
+    constructor(h: number, m: number) {
 
+    }
+}
+// 6.1 静态部分与实例部分
+interface ClockConstructor {
+    new { hour: number, minute: number }:ClockInterface;
+}
+interface ClockInterface {
+    tick();
+}
+function createClock(ctor: ClockConstructor, hour: number, minute: number): ClockInterface {
+    return new ctor(hour, mintue;)
+}
+class DigitalClock implements ClockINterface {
+    constructor(h: number, m: number) { }
+    tick() {
+        console.log("ha ha");
+    }
+}
+class AnalogClock implements ClockInterface {
+    constructor(h: number, m: number) { }
+    tick() {
+        console.log('he he');
+    }
+}
+// 7.0 继承接口
+interface Shape {
+    color: string;
+}
+interface Square extends Shape {
+    sideLenth: number;
+}
+let square = <Square>{};
+square.color = 'blue';
+square.sideLenth = 10;
+//  一个接口可以继承多个接口，创建多个接口的合成接口
+interface Shape {
+    color: string;
+}
+interface PenStroke {
+    penWidth: number;
+}
+interface Square extends Shape, PenStroke {
+    sideLenth: number;
+}
+let squareone = <Square>{};
+squareone.color = 'blue';
+squareone.sideLenth = 10;
+squareone.penWidth = 5.0;
+// 8.0 混合类型
+interface Counter {
+    (start:number):string;
+    interval:number;
+    reset():void;
+}
+function getCounter():Counter{
+    let counter = <Counter>function(start:number){};
+    counter.interval = 123;
+    counter.reset = function(){};
+    return counter;
+}
+let c = getCounter();
+c(10);
+c.reset();
+c.interval = 5.0;
+// 9.0 接口继承类
+class Control {
+    private state:any;
+}
+interface SelectableControl extends Control {
+    select():void;
+}
+class Button extends Control implements SelectableControl {
+    select(){}
+}
+class TextBox extends Control {
+
+} 
+//error "Image类型缺少"state"属性".
+class Image implements SelectableControl{
+    select() {}
+}
