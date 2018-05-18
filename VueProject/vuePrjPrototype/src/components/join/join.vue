308c184f946deee2cf284d5e6227f7a0bd11b3df
Small-inn <1457276774@qq.com>
2018-05-19 00:08:26 +0800

    0519

--- VueProject/vuePrjPrototype/src/components/join/join.vue
+++ VueProject/vuePrjPrototype/src/components/join/join.vue
@@ -1,16 +1,31 @@
 <template>
-    <div>
-        <Button>Default</Button>
-        <Button type="primary">Primary</Button>
-        <Button type="ghost">Ghost</Button>
-        <Button type="dashed">Dashed</Button>
-        <Button type="text">Text</Button>
-        <br><br>
-        <Button type="info">Info</Button>
-        <Button type="success">Success</Button>
-        <Button type="warning">Warning</Button>
-        <Button type="error">Error</Button>
-        <button @click="jump" class="btn">点击</button>    
+    <div class="screen">
+      <div class="primary-level">
+        <div class="img-con">
+          <img src="" alt="" >
+        </div>
+        <p>初级俱乐部（人数上限100）</p>
+      </div>
+      <div class="middle-level">
+        <div class="img-con">
+          <img src="" alt="" >
+        </div>
+        <p>中级俱乐部（人数上200）</p>
+        <div class="extend">
+          <span>金币10,000</span>
+          <span>扩充到中级俱乐部</span>
+        </div>    
+      </div>
+      <div class="high-level">
+        <div class="img-con">
+          <img src="" alt="" >
+        </div>
+        <p>高级俱乐部（人数上限1000）</p>
+        <div class="extend">
+          <span>金币10,000</span>
+          <span>扩充到高级俱乐部</span>
+        </div>
+      </div>
     </div>
 </template>
 <script>
@@ -18,16 +33,60 @@ export default {
   data() {
     return {};
   },
-  methods: {
-    jump() {
-      this.$router.push({ name: "rater" });
-    }
-  }
+  methods: {}
 };
 </script>
-<style lang="less" scoped>
-    .btn{
-        width: 2rem;
-        height: 2rem;
-    }
+<style scoped>
+.primary-level,
+.middle-level,
+.high-level{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+.primary-level {
+  width: 100%;
+  margin-top: 51px;
+}
+.middle-level,
+.high-level {
+  margin-top: 62px;
+}
+.primary-level .img-con,.middle-level .img-con,.high-level .img-con {
+  width: 140px;
+  height: 90px;
+}
+.primary-level,
+.middle-level,
+.high-level p {
+  font-size: 14px;
+  text-align: center;
+}
+.primary-level p{
+  color: #65A085;
+}
+.middle-level P{
+  color: #f88c30;
+}
+.high-level p{
+  color: #E61B14;
+}
+.extend {
+  padding: 0 18px 0 16px;
+  width: 343px;
+  height: 40px;
+  line-height: 40px;
+  color:#fff;
+  font-size: 13px;
+  margin-top: 10px;
+  display: flex;
+  justify-content: space-between;
+}
+.middle-level .extend {
+  background-color: #f88c30;
+}
+.high-level .extend {
+  background-color: #E61B14;
+}
 </style>
